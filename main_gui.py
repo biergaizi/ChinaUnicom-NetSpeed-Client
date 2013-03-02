c2a84f1efb4525f4e622dd248c38864c2171019b
Tom Li <biergaizi2009@gmail.com>
2013-03-02 13:07:26 +0800

    分离 NetSpeed 模块

--- main_gui.py
+++ main_gui.py
@@ -2,11 +2,9 @@
 import sys
 import os
 sys.path.append(os.getcwd() + "/ui")
-import random
-import httplib2
-from bs4 import BeautifulSoup
 from PyQt4 import QtCore, QtGui
 from main_ui import Ui_MainWindow
+from netspeed import NetSpeed
 
 
 class MainWindow(QtGui.QMainWindow, Ui_MainWindow):
@@ -23,17 +21,17 @@ class MainWindow(QtGui.QMainWindow, Ui_MainWindow):
         self.timer.stop()
         self.myNetSpeed = NetSpeed()
         self.status_display()
-        self.timer.start(32 * 60 * 1000) 
+        self.timer.start(32 * 60 * 1000)
         QtCore.QObject.connect(self.timer, QtCore.SIGNAL("timeout()"), self.refresh)
 
     def status_display(self):
         if not self.myNetSpeed.status:
-            self.label.setText('<html><head/><body><p align="center">你未处于提速状态</p><p align="center">提速前：%s M；提速后：%s M</p><p align="center">剩余提速时长：%s 小时</p></body></html>' 
+            self.label.setText('<html><head/><body><p align="center">你未处于提速状态</p><p align="center">提速前：%s M；提速后：%s M</p><p align="center">剩余提速时长：%s 小时</p></body></html>'
                     % (self.myNetSpeed.old_speed, self.myNetSpeed.new_speed, self.myNetSpeed.hours))
             self.btnSpeedChange.setText("提速")
             self.btnSpeedChange.setEnabled(self.myNetSpeed.hours)
         else:
-            self.label.setText('<html><head/><body><p align="center">你已处于提速状态</p><p align="center">提速前：%s M；提速后：%s M</p><p align="center">剩余提速时长：%s 小时</p></body></html>' 
+            self.label.setText('<html><head/><body><p align="center">你已处于提速状态</p><p align="center">提速前：%s M；提速后：%s M</p><p align="center">剩余提速时长：%s 小时</p></body></html>'
                     % (self.myNetSpeed.old_speed, self.myNetSpeed.new_speed, self.myNetSpeed.hours))
             self.btnSpeedChange.setText("降速")
 
@@ -55,63 +53,6 @@ class MainWindow(QtGui.QMainWindow, Ui_MainWindow):
     def refresh(self):
         self.myNetSpeed.get_info()
         self.status_display()
-
-
-class NetSpeed(object):
-    def __init__(self):
-        self.get_info()
-
-    def parse_info(self, html):
-        def clean_html(html):
-            soup = BeautifulSoup(html)
-            result = soup.find(id="webcode").string
-
-            # Remove unexcepted ";" under OS X.
-            return result.replace(";", "")
-
-        html = clean_html(html)
-        html = html.split('&')
-        info = {}
-        for i in html:
-            tag, value = i.split('=')
-            info[tag] = value
-
-        return info
-
-    def speed_up(self):
-        h = httplib2.Http()
-        resp, content = \
-                h.request("http://bj.wokuan.cn/web/improvespeed.php?ContractNo=%s&up=%s&old=%s&round=%s"
-                        % (self.id, self.new_speed_id, self.old_speed_id, random.randint(0, 100)))
-
-        self.get_info()
-        content = content.decode("utf-8")
-        return "success&00000000" in content
-
-    def speed_down(self):
-        h = httplib2.Http()
-        resp, content = \
-                h.request("http://bj.wokuan.cn/web/lowerspeed.php?ContractNo=%s&round=%s"
-                        % (self.id, random.randint(0, 100)))
-
-        self.get_info()
-        content = content.decode("utf-8")
-        return "success&00000000" in content
-
-    def get_info(self):
-        h = httplib2.Http()
-        resp, content = h.request("http://bj.wokuan.cn/web/startenrequest.php")
-
-        content = content.decode("utf-8")
-        info = self.parse_info(content)
-
-        self.id = info['cn']
-        self.status = int(info['stu'])
-        self.old_speed = info['os']
-        self.old_speed_id = info['old']
-        self.new_speed = info['up']
-        self.new_speed_id = info['gus']
-        self.hours = float(info['glst'])
 
 
 App = QtGui.QApplication(sys.argv)
